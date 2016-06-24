dddc0114fe87da0232ea95804722a78a9d652693
Dingzeyu Li <dli@cs.columbia.edu>
2016-06-24 17:14:45 -0400

    tetgen file io: quadratic elements

--- mesh/readFACE.m
+++ mesh/readFACE.m
@@ -18,12 +18,13 @@ function [F,B,P] = readFACE(filename,varargin)
 
   force_no_boundary = false;
   parent_tetrahedra = false;
+  element_order = 1;
   
   % default values
   % Map of parameter names to variable names
   params_to_variables = containers.Map( ...
-    {'ForceNoBoundary', 'ParentTetrahedra'}, ...
-    {'force_no_boundary', 'parent_tetrahedra'} );
+    {'ForceNoBoundary', 'ParentTetrahedra', 'ElementOrder'}, ...
+    {'force_no_boundary', 'parent_tetrahedra', 'element_order'} );
   v = 1;
   while v <= numel(varargin)
     param_name = varargin{v};
@@ -69,14 +70,24 @@ function [F,B,P] = readFACE(filename,varargin)
   else
     parser = '%d %d %d %d %d %d';
     num_items = 4;
+    face_index_range = num_items;
+    
+    if (element_order == 2)
+      parser = [parser ' %d %d %d'];
+      num_items = num_items + 3;      
+      face_index_range = num_items;
+    end
+      
     if (boundary_markers ~= 0)
       parser = [parser ' %d'];
-      num_items = 5;
+      num_items = num_items + 1;
+      boundary_range = num_items;
     end
     
     if parent_tetrahedra
       parser = [parser ' %d %d'];
-      num_items = 7;      
+      num_items = num_items + 2;
+      parent_range = num_items;
     end
   
     F = fscanf(fp,parser,num_items*sizeF);
@@ -84,16 +95,16 @@ function [F,B,P] = readFACE(filename,varargin)
   
     F = reshape(F,num_items,sizeF)';
     B = [];
-    P=[];
+    P = [];
     if boundary_markers
-      B = F(:,5);
+      B = F(:,boundary_range);
     end
     
     if parent_tetrahedra
-      P = F(:,6:7);
+      P = F(:,parent_range-1:parent_range);
     end
   
     % get rid of indices and boundary markers and make one indexed
-    F = F(:,2:4);
+    F = F(:,2: face_index_range);
   end
 end
